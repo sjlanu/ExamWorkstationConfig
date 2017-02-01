55c55f2f08b2758666d55ba1226816e08f689ffd
Sami Lanu <sami.lanu@tut.fi>
2017-02-01 12:48:55 +0200

    Added new cleaning script with logging, removed obsolete scripts

---
+++ Scripts/Del-exam_prof-c_temp.ps1
@@ -0,0 +1,43 @@
+# Authors
+# -------
+#
+# Sami Lanu <sami.lanu@tut.fi>
+#
+# Copyright
+# ---------
+#
+# Copyright 2017 Tampere University of Technology
+# Remove Exam user profile and c:\temp at logoff
+# set logpath
+# set profile
+$profile = 'c:\users\examuser'
+$logpath = '\\path\to\log'
+# Set delprof2 path
+$scriptpath = '\\path\to\scripts'
+# set touch function
+function touch {set-content -Path ($args[0]) -Value ($null)}
+# set file to be touched
+$touchfile = 'c:\exam-delprof.touchfile'
+touch $touchfile
+# set scriptpath to currentdir
+# $scriptpath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+# Run delprof2
+Start-Process -Filepath $scriptpath\delprof2.exe -ArgumentList ('/q','/u') -NoNewWindow -Wait
+# remove c:\temp
+remove-item -recurse -force c:\temp
+# create c:\temp
+new-item c:\temp -type directory -force
+# set permissions to c:\temp
+cmd /c icacls.exe "c:\temp" /grant "*S-1-5-11:(OI)(CI)(M)"
+# check that c:\users\tut-exam not exist
+$date = Get-Date -format M-d-yyyy-hh-mm
+if ((test-path $profile)) {
+touch $logpath\examuser-exists-$env:computername-$date.txt
+}
+# check that c:\temp is empty
+$dircontents = get-childitem c:\temp | measure-object
+if ($dircontents.count -ne 0) {
+touch $logpath\c-temp-not-empty-$env:computername-$date.txt
+}
+# remove $touchfile
+remove-item -force $touchfile
--- Scripts/ProfileDelete.cmd
+++ Scripts/ProfileDelete.cmd
@@ -6,10 +6,10 @@
 # Copyright
 # ---------
 #
-# Copyright 2016 Tampere University of Technology
+# Copyright 2017 Tampere University of Technology
 @echo off
 rem Content dir with "\" in the end
 rem "%~dp0" = directory of this script
 set CONTENTDIR=%~dp0
 
-%CONTENTDIR%DelProf2.exe /q /u
+powershell.exe -executionpolicy bypass -file %CONTENTDIR%Del-exam_prof-c_temp.ps1
