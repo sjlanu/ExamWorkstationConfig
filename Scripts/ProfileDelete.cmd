da6d11aacc65c0abdf076732b27dd1ecaa6e8466
Sami Lanu <sami.lanu@tut.fi>
2017-02-01 15:51:33 +0200

    Fixed ProfileDelete.cmd script

--- Scripts/ProfileDelete.cmd
+++ Scripts/ProfileDelete.cmd
@@ -11,5 +11,10 @@
 rem Content dir with "\" in the end
 rem "%~dp0" = directory of this script
 set CONTENTDIR=%~dp0
+set MYSCRIPTS="c:\myscipts"
+set UNCPATH="\\path\to"
+set SCRIPT="Del-exam_prof-c_temp.ps1"
 
-powershell.exe -executionpolicy bypass -file %CONTENTDIR%Del-exam_prof-c_temp.ps1
+if not exist %MYSCRIPTS% mkdir %MYSCRIPTS%
+copy /Y %UNCPATH%\%SCRIPT% %MYSCRIPTS%
+powershell.exe -executionpolicy bypass -file %MYSCRIPTS%\%SCRIPT%
